fafe23a1ace6f6ceccdaf558d68b8079755da0ce
mcekiera <marcin.cekiera@gmail.com>
2016-03-28 15:04:19 +0200

    working complex without content

--- src/Model/Constructs/test.java
+++ src/Model/Constructs/test.java
@@ -1,12 +1,13 @@
 package Model.Constructs;
 
 import Model.Expression;
+import Model.ExpressionBuilder;
 
 public class test {
 
     public static void main(String[] args) {
 
-        Expression expression = new Expression("(?<=aaa)({4,6})[]]\\1");
+        Expression expression = (new ExpressionBuilder()).toExpression("(?<=aaa)({4,6})[]]\\1");
         expression.setCurrentMatch("marcin.cekiera@gmail.com");
         int i = 0;
         for(Construct construct : expression) {
