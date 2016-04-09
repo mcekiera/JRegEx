930e31124ed67f3ea9a1db5ab4439e8654d09396
mcekiera <marcin.cekiera@gmail.com>
2016-04-09 13:46:43 +0200

    working color changing on many levels

--- src/View/ColorPalette.java
+++ src/View/ColorPalette.java
@@ -1,11 +1,13 @@
 package View;
 
 import java.awt.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ColorPalette {
     private final static ColorPalette INSTANCE = new ColorPalette();
     private static boolean matching = true;
-    private static boolean group = true;
+    private static List<Boolean> groups = getGroup();
 
     private ColorPalette() {}
 
@@ -15,7 +17,7 @@ public class ColorPalette {
 
     public void reset() {
         matching = true;
-        group = true;
+        groups = getGroup();
         System.out.println("ui");
     }
 
@@ -25,11 +27,26 @@ public class ColorPalette {
             case 0:
                 matching = !matching;
                 return HLColor.getColor(HLColor.MATCH_ONE,matching);
-
+            case 1:
+                groups.set(i,!groups.get(i));
+                return HLColor.getColor(HLColor.GROUP1,groups.get(i));
+            case 2:
+                groups.set(i,!groups.get(i));
+                return HLColor.getColor(HLColor.GROUP2,groups.get(i));
+            case 3:
+                groups.set(i,!groups.get(i));
+                return HLColor.getColor(HLColor.GROUP3,groups.get(i));
             default:
-                group = !group;
-                    return HLColor.getColor(HLColor.GROUP1,group);
+                return HLColor.getColor(HLColor.GROUP1,groups.get(i));
         }
+    }
+
+    private static List<Boolean> getGroup() {
+        List<Boolean> list = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            list.add(true);
+        }
+        return list;
     }
 
 }
--- src/View/HLColor.java
+++ src/View/HLColor.java
@@ -5,6 +5,8 @@ import java.awt.*;
 public enum HLColor {
 
     GROUP1(new Color(75,255,75),new Color(150,255,150)),
+    GROUP2(new Color(222,184,45), new Color(255,211,155)),
+    GROUP3(new Color(173,225,45),new Color(202,255,118)),
     MATCH_ONE(new Color(75,150,255),new Color(100,200,255));
 
     Color c1;
